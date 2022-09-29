16b997aeef4c9ee1e2ed1c5d1b519f296bbbd503
jbundziow <jakub.bundziow@gmail.com>
2022-09-29 19:46:28 +0200

    added import/export js exercise

---
+++ JS-Exercises/Import-Export-JS/script.js
@@ -0,0 +1,19 @@
+//MAIN FILE
+
+//normal function
+export function addNumbers(a,b) {
+    return a+b;
+}
+
+//arrow function
+export const subtractNumbers = (a,b) => a-b;
+
+//normal function
+export function hello ()
+{
+    console.log("Hello world!");
+}
+
+//variables
+export const x = 20;
+export const str = 'hehe';
---
+++ JS-Exercises/Import-Export-JS/test.js
@@ -0,0 +1,21 @@
+//SECOND FILE
+
+import { addNumbers, subtractNumbers, hello, x, str } from "./script.js";
+
+
+//using imported functions/variables
+console.log(addNumbers(2,4));
+console.log(subtractNumbers(10,1));
+hello();
+console.log(x);
+console.log(str);
+
+
+/*
+ATTENTION! YOU MUST HAVE:
+{
+  "type": "module"
+}
+
+IN YOUR 'package.json'
+*/
