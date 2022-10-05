9f21389aa90a98ed52d9ed09f6be61f3a4262a0b
jbundziow <jakub.bundziow@gmail.com>
2022-10-05 07:57:58 +0200

    completed ex2 in js course

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/91.dom,window,document-zadanie2/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/91.dom,window,document-zadanie2/script.js
@@ -1,1 +1,29 @@
-console.log('ok');
+//MY SOLUTION:
+const btn_hamburger = document.querySelector('.buttons > .btn1');
+const btn_pizza = document.querySelector('.buttons > .btn2');
+const square = document.querySelector('.square');
+const p1 = document.querySelector('.text > .p1');
+const p2 = document.querySelector('.text > .p2');
+
+
+const greeting = () => {
+console.log('cześć');
+}
+
+const changeColorOfElement = (element, color) => {
+    const item = document.querySelector(element);
+    item.style.backgroundColor = color;
+}
+
+
+function showAndHideParagraph() {
+    p1.classList.toggle('show');
+    p2.classList.toggle('show');
+}
+
+
+
+btn_hamburger.addEventListener('dblclick', greeting);
+square.addEventListener('mouseenter', function () {changeColorOfElement('.square','red')});
+square.addEventListener('mouseleave', function () {changeColorOfElement('.square','royalblue')});
+btn_pizza.addEventListener('click', showAndHideParagraph);
