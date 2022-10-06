d461a0a13d065a1ef74277c226936e7cad682263
jbundziow <jakub.bundziow@gmail.com>
2022-10-06 08:21:20 +0200

    project[106] - added the first features

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/106.projekty - konwenter stC na stF/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/106.projekty - konwenter stC na stF/script.js
@@ -1,1 +1,64 @@
-console.log('hey')
+let isCelsiusToFahrenheit = true;
+
+const btnConv = document.querySelector('.conv');
+const btnReset = document.querySelector('.reset');
+const btnChange = document.querySelector('.change');
+const enteredValue = document.querySelector('#converter');
+const resultParagraph = document.querySelector('.result');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+
+
+//console.log(enteredValue.value);
+
+
+//TODO: round to 2 numbers after . ; e.g. 25.23
+const convertTemperatures = () => {
+    if(enteredValue.value == "") {
+        resultParagraph.textContent = "You have not entered any value!";
+        resultParagraph.style.color = "red";
+        //TODO: red border and shake element!!!
+        return; //exit
+    }
+    let result;
+        if (isCelsiusToFahrenheit) {
+        result = 9/5*enteredValue.value+32;
+        resultParagraph.textContent = `${enteredValue.value}°C equals ${result}°F`;
+        }
+        else {
+            result = 5/9*(enteredValue.value-32);
+            resultParagraph.textContent = `${enteredValue.value}°F equals ${result}°C`;
+        }
+}
+
+const reset = () => {
+enteredValue.value = null;
+resultParagraph.textContent = null;
+}
+
+const changeUnit = () => {
+    if (isCelsiusToFahrenheit) {
+        one.textContent = '°F';
+        two.textContent = '°C';
+        //TODO: change min/max in number input in html (not out of range)
+        //-273.15 C is minimum
+        isCelsiusToFahrenheit = false;
+    }
+    else {
+        one.textContent = '°C';
+        two.textContent = '°F';
+        //TODO: change min/max in number input in html (not out of range)
+        isCelsiusToFahrenheit = true;
+    }
+
+    if (enteredValue.value !== "") {
+        convertTemperatures();
+    }
+    else {
+        reset();
+    }
+}
+
+btnConv.addEventListener('click', convertTemperatures);
+btnReset.addEventListener('click', reset);
+btnChange.addEventListener('click', changeUnit);
