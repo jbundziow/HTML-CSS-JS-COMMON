6ebd56d7345d1b1642f0de6a03081ae938c181a1
jbundziow <jakub.bundziow@gmail.com>
2022-10-10 08:13:14 +0200

    completed example in js about modifying attributes

--- JS-Exercises/setAttribute(), removeAttribute(), getAttribute(), hasAttribute()/script.js
+++ JS-Exercises/setAttribute(), removeAttribute(), getAttribute(), hasAttribute()/script.js
@@ -1,13 +1,38 @@
+const link = document.querySelector('a');
+const myClg = document.querySelector('.results > textarea');
 
+myClg.textContent = 'Welcome!\nLink redirect to "https://github.com/jbundziow"\n';
 
 
 //! --------- setAttribute() ----------
-
+const clickedSet = () => {
+    link.setAttribute('href','https://github.com/jbundziow');
+    myClg.textContent += 'Changed "href" to "https://github.com/jbundziow"\n';
+    scrollDownTextarea();
+}
 
 //! --------- removeAttribute() ----------
-
+const clickedRemove = () => {
+    link.removeAttribute('href');
+    myClg.textContent += 'Removed attribute "href"\n';
+    scrollDownTextarea();
+}
 
 //! --------- getAttribute() ----------
-
+const clickedGet = () => {
+    let savedAttributeContent = link.getAttribute('href');
+    myClg.textContent += `Read value: ${savedAttributeContent}\n`;
+    scrollDownTextarea();
+}
 
 //! --------- hasAttribute() ----------
+const clickedHas = () => {
+    let respond = link.hasAttribute('href');
+    myClg.textContent += `Link has attribute 'href'?: ${respond}\n`;
+    scrollDownTextarea();
+}
+
+
+   function scrollDownTextarea() {
+        myClg.scrollTop = myClg.scrollHeight;
+   }
