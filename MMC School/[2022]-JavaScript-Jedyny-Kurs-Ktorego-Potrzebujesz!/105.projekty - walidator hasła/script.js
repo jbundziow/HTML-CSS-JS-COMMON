9581dec1af46e6e7aac7c3c8f14af3ae1ecf364c
jbundziow <jakub.bundziow@gmail.com>
2022-10-12 15:01:23 +0200

    added some js

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/105.projekty - walidator hasła/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/105.projekty - walidator hasła/script.js
@@ -5,5 +5,30 @@ const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
 
+const checkPasswordStrength = () => {
+    if (pass.value.length > 0 && pass.value.length < minValue ) {
+        p.textContent = 'Twoje hasło jest za krótkie.'
+    }
+    else if (pass.value.length == 0) {
+        p.textContent = "Nie podałeś hasła..."
+    }
+    else if (!pass.value.match(letters)) {
+        p.textContent = "Twoje hasło nie zawiera liter."
+    }
+    else if (!pass.value.match(numbers)) {
+        p.textContent = "Twoje hasło nie zawiera cyfr."
+    }
+    else {
+        if (pass.value.match(special)) {
+            p.textContent = "Twoje hasło jest bardzo silne."
+        }
+        else {
+        p.textContent = "Twoje hasło jest silne."
+        }
+    }
+    // TODO: ADD 'COLOR' PROPERTY IN CSS
+    // TODO: Check result with mmc school
+}
 
-console.log('Hello world!');
+
+pass.addEventListener('keyup', checkPasswordStrength);
