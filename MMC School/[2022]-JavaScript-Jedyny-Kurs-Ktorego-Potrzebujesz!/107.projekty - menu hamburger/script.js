bdec11f819b13eb0aa7aaa8fb9ffdc1a60abbfbd
jbundziow <jakub.bundziow@gmail.com>
2022-10-14 08:21:29 +0200

    completed exercise

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/107.projekty - menu hamburger/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/107.projekty - menu hamburger/script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const menu = document.querySelector('nav ul');
+const btnExpandMenu = document.querySelector('.fa-bars');
+const btnHideMenu = document.querySelector('.fa-times');
+const bothButtons = document.querySelector('.burger');
+
+
+bothButtons.addEventListener('click', function () {
+	menu.classList.toggle('active');
+    bothButtons.classList.toggle('active');
+	btnExpandMenu.classList.toggle('hide');
+    btnHideMenu.classList.toggle('hide');
+});
+
+
+
