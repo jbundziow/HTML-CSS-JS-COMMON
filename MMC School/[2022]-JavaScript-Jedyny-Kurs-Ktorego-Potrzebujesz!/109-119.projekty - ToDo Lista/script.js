244099263d40a043c5f8a6da8df4b3b6453a602a
jbundziow <jakub.bundziow@gmail.com>
2022-10-19 22:59:03 +0200

    progress in todo-list project

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/109-119.projekty - ToDo Lista/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/109-119.projekty - ToDo Lista/script.js
@@ -1,1 +1,44 @@
-console.log('hello world');
+let todoInput;
+let errorInfo; //under input
+let addBtn;
+let ulList;
+let newTodo; //new todo as 'li'
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEEvents();
+    console.log(ulList);
+}
+
+const prepareDOMElements = () => {
+    todoInput = document.querySelector('.todo-input');
+    errorInfo = document.querySelector('.error-info');
+    addBtn = document.querySelector('.btn-add');
+    ulList = document.querySelector('.todolist > ul');
+}
+
+
+const prepareDOMEEvents = () => {
+    addBtn.addEventListener('click', addNewTodo);
+}
+
+const addNewTodo = () => {
+    if (todoInput.value !== '') {
+        newTodo = document.createElement('li');
+        newTodo.textContent = todoInput.value;
+        ulList.append(newTodo);
+
+        todoInput.value = '';
+        errorInfo.textContent = '';
+    }
+    else {
+        errorInfo.textContent = 'Wpisz treść zadania';
+    }
+}
+
+
+
+
+
+//ADDEVENTLISTENERS
+document.addEventListener('DOMContentLoaded', main);
