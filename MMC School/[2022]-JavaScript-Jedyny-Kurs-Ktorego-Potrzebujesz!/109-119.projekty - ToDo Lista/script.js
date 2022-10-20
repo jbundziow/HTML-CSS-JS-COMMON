ec735784052c90c7ddbfe7356b6693da94c6a9b6
jbundziow <jakub.bundziow@gmail.com>
2022-10-20 11:15:11 +0200

    added createTools function

--- MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/109-119.projekty - ToDo Lista/script.js
+++ MMC School/[2022]-JavaScript-Jedyny-Kurs-Ktorego-Potrzebujesz!/109-119.projekty - ToDo Lista/script.js
@@ -7,7 +7,6 @@ let newTodo; //new todo as 'li'
 const main = () => {
     prepareDOMElements();
     prepareDOMEEvents();
-    console.log(ulList);
 }
 
 const prepareDOMElements = () => {
@@ -34,6 +33,49 @@ const addNewTodo = () => {
     else {
         errorInfo.textContent = 'Wpisz treść zadania';
     }
+     createToolArea(newTodo);
+}
+
+const createToolArea = (newTodo) => {
+    
+//create div
+const toolsDiv = document.createElement('div');
+toolsDiv.classList.add('tools');
+
+//button complete
+const buttonComplete = document.createElement('button');
+buttonComplete.classList.add('complete');
+const iconComplete = document.createElement('i');
+iconComplete.classList.add('fas','fa-check');
+buttonComplete.append(iconComplete);
+
+//button edit
+const buttonEdit = document.createElement('button');
+buttonEdit.classList.add('edit');
+buttonEdit.append('EDIT');
+
+//button delete
+const buttonDelete = buttonComplete.cloneNode(true); //using cloneNode method to not to edit initial buttonComplete
+buttonDelete.classList.replace('complete','delete');
+const iconDelete = buttonDelete.querySelector('i');
+iconDelete.classList.replace('fa-check', 'fa-times');
+
+//append buttons to myDiv
+toolsDiv.append(buttonComplete, buttonEdit, buttonDelete);
+
+//apend created <div> to lastLi
+newTodo.append(toolsDiv);
+
+//EASIER WAY
+/*
+newTodo.innerHTML += `
+    <div class="tools">
+    <button class="complete"><i class="fas fa-check"></i></button>
+    <button class="edit">EDIT</button>
+    <button class="delete"><i class="fas fa-times"></i></button>
+    </div>
+    `;
+*/
 }
 
 
