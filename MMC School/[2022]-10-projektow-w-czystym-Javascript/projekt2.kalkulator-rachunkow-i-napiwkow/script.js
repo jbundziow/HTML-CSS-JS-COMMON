61dbbc240b67279559c775b0a713672759f30d0f
jbundziow <jakub.bundziow@gmail.com>
2022-10-29 18:51:16 +0200

    completed js

--- MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt2.kalkulator-rachunkow-i-napiwkow/script.js
+++ MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt2.kalkulator-rachunkow-i-napiwkow/script.js
@@ -9,22 +9,6 @@ const resultInfo = document.querySelector('.result-info');
 const errorInfo = document.querySelector('.error-info');
 const finalCost = document.querySelector('.final-cost');
 
-//! DELETE IT
-// let x = false;
-// setInterval(() => {
-//     if (x) {
-//         resultInfo.style.display = 'block';
-//         errorInfo.style.display = 'none';
-//         x = false;
-//     }
-//     else {
-//         resultInfo.style.display = 'none';
-//         errorInfo.style.display = 'block';
-//         x = true;
-//     }
-// }, 1000);
-//! XXXXXXXXXXX
-
 //functions
 function isEmpty(str) {
     return !str.trim().length;
@@ -33,16 +17,22 @@ function isEmpty(str) {
 const inputsAreEmpty = () => {
     return isEmpty(price.value) || isEmpty(people.value) || parseFloat(tip.value) === 0 ? true : false;
 }
-//TODO: function to check if values are correct (price minimum 0; people === int)
+
+const inputsValuesAreCorrect = () => {
+    const val_price = parseFloat(price.value);
+    const val_people = parseFloat(people.value);
+    return val_price > 0 && Number.isInteger(val_people) && val_people > 0 ? true : false;
+}
+
+
 const countFinalCost = () => {
-    if (inputsAreEmpty()) {
+    if (inputsAreEmpty() || !inputsValuesAreCorrect()) {
         resultInfo.style.display = 'none';
         errorInfo.style.display = 'block';
-        //TODO: shake form
     }
     else {
         const totalPrice = (1 + parseFloat(tip.value)) * parseFloat(price.value) / parseFloat(people.value);
-        finalCost.textContent = totalPrice;
+        finalCost.textContent = totalPrice.toFixed(2);
         resultInfo.style.display = 'block';
         errorInfo.style.display = 'none';
     }
