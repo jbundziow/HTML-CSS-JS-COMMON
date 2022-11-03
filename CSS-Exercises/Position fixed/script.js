e5275a49aa7ba25ac398fe20f09934e0fb0e57dc
jbundziow <jakub.bundziow@gmail.com>
2022-11-03 08:08:59 +0100

    added new css exercise

---
+++ CSS-Exercises/Position fixed/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const homeSection = document.querySelector('#home')
+const aboutUsSection = document.querySelector('#about-us')
+const workSection = document.querySelector('#work')
+const contactSection = document.querySelector('#contact')
+
+console.log(homeSection);
+console.log(aboutUsSection);
+console.log(workSection);
+console.log(contactSection);
+
+//window.scrollTo(0, document.querySelector(".anchor").offsetTop);
