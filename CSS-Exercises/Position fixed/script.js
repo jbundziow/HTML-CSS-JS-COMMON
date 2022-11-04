5bb65629fac48d2649acc1059364bd8d2cb8deca
jbundziow <jakub.bundziow@gmail.com>
2022-11-04 19:54:15 +0100

    final

--- CSS-Exercises/Position fixed/script.js
+++ CSS-Exercises/Position fixed/script.js
@@ -5,9 +5,55 @@ const aboutUsSection = document.querySelector('#about-us')
 const workSection = document.querySelector('#work')
 const contactSection = document.querySelector('#contact')
 
-console.log(homeSection);
-console.log(aboutUsSection);
-console.log(workSection);
-console.log(contactSection);
+//buttons nav
+const homeBtnNav = document.querySelector('.home-link')
+const aboutusBtnNav = document.querySelector('.aboutus-link')
+const workBtnNav = document.querySelector('.work-link')
+const contactBtnNav = document.querySelector('.contact-link')
 
-//window.scrollTo(0, document.querySelector(".anchor").offsetTop);
+//global variables
+let homeSectionDimensions, aboutUsSectionDimensions, workSectionDimensions, contactSectionDimensions;
+
+
+
+
+const getDimensionsOfElement = (element) => {
+    const topOfTheElement = $(element).offset().top;
+    const bottomOfTheElement = topOfTheElement + element.offsetHeight;
+    return [topOfTheElement, bottomOfTheElement]; //in pixels from the beggining of document
+}
+
+const HighlightNavButton = () => {
+    const scrollTop = $(window).scrollTop();
+    const middleOfTheScreen = scrollTop + window.innerHeight/2;
+
+    homeBtnNav.classList.remove('active');
+    aboutusBtnNav.classList.remove('active');
+    workBtnNav.classList.remove('active');
+    contactBtnNav.classList.remove('active');
+
+    if (middleOfTheScreen >= homeSectionDimensions[0] && middleOfTheScreen < homeSectionDimensions[1]) {
+        homeBtnNav.classList.add('active');
+    }
+    else if (middleOfTheScreen >= aboutUsSectionDimensions[0] && middleOfTheScreen < aboutUsSectionDimensions[1]) {
+        aboutusBtnNav.classList.add('active');
+    }
+    else if (middleOfTheScreen >= workSectionDimensions[0] && middleOfTheScreen < workSectionDimensions[1]) {
+        workBtnNav.classList.add('active');
+    }
+    else if (middleOfTheScreen >= contactSectionDimensions[0] && middleOfTheScreen < contactSectionDimensions[1]) {
+        contactBtnNav.classList.add('active');
+    }
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    homeSectionDimensions = getDimensionsOfElement(homeSection);
+    aboutUsSectionDimensions = getDimensionsOfElement(aboutUsSection);
+    workSectionDimensions = getDimensionsOfElement(workSection);
+    contactSectionDimensions = getDimensionsOfElement(contactSection);
+    HighlightNavButton();
+});
+document.addEventListener('scroll', HighlightNavButton)
--- MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt4.magic-8-ball/script.js
+++ MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt4.magic-8-ball/script.js
@@ -1,1 +1,50 @@
 'use strict'
+
+const ballImg = document.querySelector('.ball-img > img')
+const input = document.querySelector('.question-area > input')
+const answer = document.querySelector('.question-area > .answer')
+const error = document.querySelector('.question-area > .error')
+
+const answers = ['Tak', 'Nie', 'Nie chcesz znać odpowiedzi na to pytanie...', 'Może...', 'Oczywiście, że tak!', 'Absolutnie nie!', 'Chyba tak', 'Chyba nie'];
+
+const animateBall = () => {
+    ballImg.classList.add('shake-animation')
+
+    //end animation
+    ballImg.addEventListener('animationend', () => {
+        ballImg.classList.remove('shake-animation')
+        checkQuestionCorrectness(input)
+      });
+}
+
+const checkQuestionCorrectness = (input) => {
+    if (input.value === '') {
+        error.textContent = 'Pole jest puste!'
+    }
+    else if (input.value.slice(-1) !== '?') {
+        error.textContent = 'Umieść znak "?" na końcu.'
+    }
+    else {
+        showRandomAnswer(answers, answer);
+    }
+}
+
+const showRandomAnswer = (arr,output) => {
+    output.innerHTML = '';
+    let span = document.createElement('span')
+    span.textContent = 'Odpowiedź: ';
+    output.append(span);
+    output.append(arr[getRandomInt(arr.length)])
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+ballImg.addEventListener('click', () => {
+    answer.textContent = '';
+    error.textContent = '';
+    animateBall()
+})
+
