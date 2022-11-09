8e5da4e2f76a8d876c4520927213d274b5c09080
jbundziow <jakub.bundziow@gmail.com>
2022-11-09 08:12:53 +0100

    finished project

--- MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt8.menedzer-finansow/script.js
+++ MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt8.menedzer-finansow/script.js
@@ -2,7 +2,7 @@
 
 const incomeSection = document.querySelector('.income-area')
 const expensesSection = document.querySelector('.expenses-area')
-const availableMomey = document.querySelector('.available-money')
+const availableMoney = document.querySelector('.available-money')
 const addTransactionPanel = document.querySelector('.add-transaction-panel')
 
 const nameInput = document.querySelector('#name')
@@ -20,6 +20,7 @@ const darkColorBtn = document.querySelector('.dark')
 
 
 let transactionID = 0;
+let moneySumArr = [0];
 
 
 
@@ -35,13 +36,20 @@ const closeTransactionPanel = () => {
 
 const addTransaction = (name, amount, category) => {
     if (name !== '' && amount !== '' && parseFloat(amount) !== 0 && category !== 'none') {
-
+        if (parseFloat(amount) > 0 && category !== 'income') {
+            //user selected wrong category
+            alert('Wydatek nie może być dodatnią kwotą. Postaw znak "-" na początku kwoty lub zmień kategorię.')
+        }
+        else if (parseFloat(amount) < 0 && category === 'income') {
+            alert('Ujemna kwota nie może być zapisana w kategorii "Przychód".')
+        }
+        else {
         const transactionDiv = document.createElement('div');
         transactionDiv.classList.add('transaction');
         transactionDiv.setAttribute('id',transactionID)
         transactionDiv.innerHTML = `
         <p class="transaction-name"><i class="${getCategoryIcon(category)}"></i> ${name}</p>
-        <p class="transaction-amount">${amount}zł 
+        <p class="transaction-amount">${parseFloat(amount).toFixed(2)}zł 
         <button class="delete" onclick=deleteTransaction(${transactionID})><i class="fas fa-times"></i></button>
         </p>
         `
@@ -54,7 +62,10 @@ const addTransaction = (name, amount, category) => {
         }
 
         transactionID++
+        moneySumArr.push(parseFloat(amount))
+        updateAvailableFunds()
         closeTransactionPanel();
+    }
     }
     else {
         alert('Uzupełnij poprawnie wszystkie pola!')
@@ -94,20 +105,43 @@ const getCategoryIcon = (categoryValue) => {
     return result;
 }
 
-//TODO: change name 'element' to something more suitable
+
 const deleteTransaction = (id) => {
     const transactions = document.querySelectorAll('.transaction');
-    transactions.forEach(element => {
-        if (parseInt(element.getAttribute('id')) === id) {
-            element.remove()
+    transactions.forEach(transaction => {
+        if (parseInt(transaction.getAttribute('id')) === id) {
+            const amountOfThisTransaction = transaction.querySelector('.transaction-amount')
+            const amountOfThisTransactionAsFloat = parseFloat(amountOfThisTransaction.textContent.trim().slice(0, -2))
+            moneySumArr.push((-1)*amountOfThisTransactionAsFloat)
+            updateAvailableFunds()
+            transaction.remove()
         }
     });
 }
 
 
-//TODO: function deleteAllTransactions
+const deleteAllTransactions = () => {
+    incomeSection.innerHTML = '<h3>Przychód:</h3>'
+    expensesSection.innerHTML = '<h3>Wydatki:</h3>'
+    moneySumArr = [0]
+    updateAvailableFunds()
+}
 
-//TODO: available funds count
+
+const updateAvailableFunds = () => {
+    let sum = 0;
+    for (const element of moneySumArr) {
+        sum += element;
+    }
+    if (sum !== 0) {
+    availableMoney.textContent = sum.toFixed(2) + 'zł'
+    }
+    else {
+        availableMoney.textContent = '0zł'
+    }
+}
+
+
 
 
 
@@ -133,10 +167,17 @@ const changeStyle = (color) => {
 }
 
 
+const keyPress = (e) => {
+    if(e.key === "Escape" && addTransactionPanel.style.display === 'flex') {
+        closeTransactionPanel()
+    }
+}
+
+
 addTransactionBtn.addEventListener('click', openTransactionPanel)
 cancelBtn.addEventListener('click', closeTransactionPanel)
 saveBtn.addEventListener('click',() =>{addTransaction(nameInput.value, amountInput.value, categorySelect.value)})
+deleteAllBtn.addEventListener('click', deleteAllTransactions)
 lightColorBtn.addEventListener('click', () => {changeStyle('light')})
 darkColorBtn.addEventListener('click', () => {changeStyle('dark')})
-
-
+document.addEventListener('keydown', keyPress) //close transaction panel after pressing 'Esc'
