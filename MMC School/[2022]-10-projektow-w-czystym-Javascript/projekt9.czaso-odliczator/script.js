d8929c674817b866ceb212bf982fe0412f9e5ce5
jbundziow <jakub.bundziow@gmail.com>
2022-11-11 16:02:56 +0100

    final version

--- MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt9.czaso-odliczator/script.js
+++ MMC School/[2022]-10-projektow-w-czystym-Javascript/projekt9.czaso-odliczator/script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const settingsBtn = document.querySelector('.settings-btn')
+
 //settings panel
 const settingsPanel = document.querySelector('.settings')
 const eventNameInput = document.querySelector('#event-name')
@@ -14,28 +15,53 @@ const saveEventBtn  = document.querySelector('.save')
 //main window
 const imageContainer = document.querySelector('.image-section')
 const eventName = document.querySelector('.event')
+const yearsCounter = document.querySelector('.years-count')
+const monthsCounter = document.querySelector('.months-count')
 const daysCounter = document.querySelector('.days-count')
 const hoursCounter = document.querySelector('.hours-count')
 const minutesCounter = document.querySelector('.minutes-count')
 const secondsCounter = document.querySelector('.seconds-count')
 
+//GLOBAL VARIABLES
+let savedEventYear, savedEventMonth, savedEventDay;
+let counterInterval;
 
-const showSettingsPanel = () => {
+
+const toggleSettingsPanel = () => {
     settingsPanel.classList.toggle('active')
 }
 
+
 const saveNewEvent = () => {
+    
     if (enteredValuesAreCorrect()) {
         //set event name
         eventName.textContent = eventNameInput.value;
 
         //set new image
         imageContainer.style.backgroundImage = `url("${eventImageInput.value}")`
+        //example of other image (just copy and paste that link)
         // https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cec4e0dd-b697-4bbc-8ba8-03157f5c55be/dcp11s8-cc1cbe55-1012-4fdf-9540-5cf3e81d921f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NlYzRlMGRkLWI2OTctNGJiYy04YmE4LTAzMTU3ZjVjNTViZVwvZGNwMTFzOC1jYzFjYmU1NS0xMDEyLTRmZGYtOTU0MC01Y2YzZTgxZDkyMWYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9i2PHei7B6h76bP6D3OG5tfFpbAx1N695H_jqXfB4A8
 
-        console.log(Date.now()); //now
-        console.log(new Date('2022-11-09').getTime()); //later
-        //TODO: check if miliseconds are not a negative number
+
+        //start counting
+        savedEventYear = eventYearInput.value;
+        savedEventMonth = eventMonthInput.value;
+        savedEventDay = eventDayInput.value;
+
+        if (counterInterval) {clearInterval(counterInterval)} //firstly, stop last setInterval function
+         counterInterval = setInterval(() => {updateCounter(savedEventYear, savedEventMonth, savedEventDay)}, 1000);
+         toggleSettingsPanel();
+
+    }
+    else {
+        clearInterval(counterInterval);
+        yearsCounter.textContent = '##'
+        monthsCounter.textContent = '##'
+        daysCounter.textContent = '##'
+        hoursCounter.textContent = '##'
+        minutesCounter.textContent = '##'
+        secondsCounter.textContent = '##'
     }
 }
 
@@ -44,12 +70,11 @@ const enteredValuesAreCorrect = () => {
     const month = parseFloat(eventMonthInput.value);
     const year = parseFloat(eventYearInput.value);
 
+    const d_now = new Date(); //now
+    const d_event = new Date(year, month-1, day) //entered event date by user
+
     if (!Number.isInteger(day) || !Number.isInteger(month) || !Number.isInteger(year)) {
         alert('Wprowadź liczby całkowite!')
-        return false;
-    }
-    else if (year < 2022) {
-        alert('Wprowadź poprawny rok!')
         return false;
     }
     else if (month < 1 || month > 12) {
@@ -66,6 +91,10 @@ const enteredValuesAreCorrect = () => {
     }
     else if (eventImageInput.value === '') {
         alert('Wprowadź URL do obrazka!')
+        return false;
+    }
+    else if (d_event - d_now < 0) {
+        alert('Wprowadziłeś przeszłą datę!')
         return false;
     }
     else {
@@ -121,17 +150,27 @@ const daysInMonth = (monthAsInt, yearAsInt) => {
 }
 
 
-const updateCounter = (ms) => {
-    //TODO - display days, hours, minutes, seconds on main screen
-	
-	const diff = new moment.duration(ms);
-	console.log(diff.asDays());     // # of days in the duration
-	console.log(diff.asHours());    // # of hours in the duration
-	console.log(diff.asMinutes());  // # of minutes in the duration
+const updateCounter = (year, month, day) => {
+    const diff = countDifferenceInMiliseconds(year,month,day);
+
+    const diffDuration = new moment.duration(diff);
+    yearsCounter.textContent = diffDuration._data.years;
+    monthsCounter.textContent = diffDuration._data.months;
+    daysCounter.textContent = diffDuration.days();
+    hoursCounter.textContent = diffDuration.hours();
+    minutesCounter.textContent = diffDuration.minutes();
+    secondsCounter.textContent = diffDuration.seconds();
 }
 
-settingsBtn.addEventListener('click', showSettingsPanel)
-saveEventBtn.addEventListener('click', saveNewEvent)
-// setInterval(() => {updateCounter(?????????????)}, 1000);
+const countDifferenceInMiliseconds = (year, month, day) => {
+    const msNow = Date.now();
+    const msEvent = new Date(`${year}-${month}-${day}`).getTime();
+    const userOffsetAsMinutes = new Date(`${year}-${month}-${day}`).getTimezoneOffset();
+    const msDifference = (msEvent +userOffsetAsMinutes*60*1000) - msNow;
+    return msDifference;
+}
 
-updateCounter(259200000)
+settingsBtn.addEventListener('click', toggleSettingsPanel)
+saveEventBtn.addEventListener('click', saveNewEvent)
+
+saveNewEvent()
