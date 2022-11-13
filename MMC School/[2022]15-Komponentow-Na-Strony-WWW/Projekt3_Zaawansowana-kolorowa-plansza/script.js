587dc3e39abac4f09469186f4702e78c15f6257f
jbundziow <jakub.bundziow@gmail.com>
2022-11-13 13:30:28 +0100

    initial files

---
+++ MMC School/[2022]15-Komponentow-Na-Strony-WWW/Projekt3_Zaawansowana-kolorowa-plansza/script.js
@@ -0,0 +1,10 @@
+const box = document.querySelector('.box')
+const speedBtns = document.querySelectorAll('[data-setting="speed"]')
+const colorBtns = document.querySelectorAll('[data-setting="color"]')
+const slider = document.querySelector('#slider')
+const sliderInfo = document.querySelector('.slider-info')
+const squares = 800
+
+let sliderValue = 70
+let range = 360
+
