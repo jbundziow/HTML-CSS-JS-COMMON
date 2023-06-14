0ae6784340fa9d9d568ab49fda753ad2b45c21c3
jbundziow <jakub.bundziow@gmail.com>
2023-06-14 23:44:16 +0200

    added comments to further exercises

--- NodeJS/Exercises/numbers-api-fetch/app.js
+++ NodeJS/Exercises/numbers-api-fetch/app.js
@@ -2,8 +2,14 @@ const fetch = require('node-fetch');
 
 
 
-
+//EXERCISE 1
+//process.argv
 fetch('http://numbersapi.com/2022/year?json')
 .then(response => {return response.json()})
 .then(data => console.log(data))
 .catch(err => console.log(err));
+
+
+//EXERCISE 2
+//trivia, math, year
+//http://numbersapi.com/${number}/${type}?json'
