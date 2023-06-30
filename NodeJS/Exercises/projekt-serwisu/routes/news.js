bde9456a615ad55bbecc3bc3e13148efb9f268a1
jbundziow <jakub.bundziow@gmail.com>
2023-06-30 20:00:32 +0200

    added news page

--- NodeJS/Exercises/projekt-serwisu/routes/news.js
+++ NodeJS/Exercises/projekt-serwisu/routes/news.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config');
+const db = require('./database');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'News' });
+  db(config.database.uri, 'showAll')
+  .then(data => {
+    res.render('news', { title: 'News', db: data});
+  })
+  
 });
 
 module.exports = router;
