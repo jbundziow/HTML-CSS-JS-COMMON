cf6533446a64ced04acdb6994dcd04aba91716c1
jbundziow <jakub.bundziow@gmail.com>
2023-08-06 18:32:58 +0200

    added logout btn

--- NodeJS/Exercises/simple-online-shop/utilities/database.js
+++ NodeJS/Exercises/simple-online-shop/utilities/database.js
@@ -3,24 +3,10 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'my_password',
+  password: 'test_password',
   database: 'node-online-shop',
-  port: 3307,
+  port: 3306,
 });
 
-// const fetchAll = () => {
-    
-// pool.execute(
-//   'SELECT * FROM `products`',
-//   function(err, results, fields) {
-//     console.log(results[0]); // results contains rows returned by server
-//     // console.log(fields); // fields contains extra meta data about results, if available
-//   }
-// );
-
-
-// }
-
-// fetchAll()
 
 module.exports = pool.promise();
