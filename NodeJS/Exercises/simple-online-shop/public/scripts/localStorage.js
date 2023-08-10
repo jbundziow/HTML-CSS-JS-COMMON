0ce37898cf185c0d13848db1917b76376b5cc420
jbundziow <jakub.bundziow@gmail.com>
2023-08-10 15:54:38 +0200

    localstorage on frontend works

--- NodeJS/Exercises/simple-online-shop/public/scripts/localStorage.js
+++ NodeJS/Exercises/simple-online-shop/public/scripts/localStorage.js
@@ -1,10 +1,27 @@
-console.log('script wokrs');
-const arr = []
-const addProductToCard = () => {
-    arr.push('x')
-    console.log('clicked');
-    localStorage.setItem('sessionData', arr);
 
-    const storedData = localStorage.getItem('sessionData');
-    console.log(typeof storedData);
+const productIDsInCart = JSON.parse(localStorage.getItem('productIDsInCart')) || [];
+const productQtyInCart = JSON.parse(localStorage.getItem('productQtyInCart')) || [];
+const addProductToCard = (id) => {
+    id = Number(id);
+    if(id === NaN) {
+        return;
+    }
+
+    if(productIDsInCart.includes(id)) {
+        const index = productIDsInCart.indexOf(id);
+        productQtyInCart[index] += 1;
+    }
+    else {
+    productIDsInCart.push(id)
+    productQtyInCart.push(1);
+    }
+    localStorage.setItem('productIDsInCart', JSON.stringify(productIDsInCart));
+    localStorage.setItem('productQtyInCart', JSON.stringify(productQtyInCart));
+
+
+    // const ids = localStorage.getItem('productIDsInCart');
+    // const qtys = localStorage.getItem('productQtyInCart');
+    
+    // console.log(JSON.parse(ids));
+    // console.log(JSON.parse(qtys));
 }
