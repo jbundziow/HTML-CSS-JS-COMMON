e3d71acfa436e4897f1f400c1a00ef079b2def6b
jbundziow <jakub.bundziow@gmail.com>
2023-08-15 15:42:32 +0200

    order adds now to a db

--- NodeJS/Exercises/simple-online-shop/controllers/api.js
+++ NodeJS/Exercises/simple-online-shop/controllers/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Product = require('../models/Product');
 const { isOrderDataValidated } = require('../data_validation/orderValidation');
+const Order = require('../models/Order');
 
 
 exports.getProductsOfIDs = (req,res,next) => {
@@ -18,14 +19,41 @@ exports.getProductsOfIDs = (req,res,next) => {
 
 exports.submitOrder = (req,res,next) => {
     const data = req.body;
+    let productsTitles = [];
+    let productsPrices = [];
+    let finalPrice = 0;
+    const orderDate = Date.now();
+    const orderStatus = 0;
     if (isOrderDataValidated(data)) {
+        Product.fetchProductsOfIds(data.productIDsInCart)
+        .then(fetchedProducts => {
+            productsTitles = fetchedProducts[0].map(product => product.title)
+            productsPrices = fetchedProducts[0].map(product => Number(product.price.toFixed(2)))
+
+            const assumedLength = data.productIDsInCart.length;
+            if(data.productQtyInCart.length === assumedLength && productsTitles.length === assumedLength && productsPrices.length === assumedLength) {
+                for (let i = 0; i < productsPrices.length; i++) {
+                    finalPrice += data.productQtyInCart[i]*productsPrices[i];
+                }
+                const newOrder = new Order(1, data.name, data.surname, data.productIDsInCart, data.productQtyInCart, productsTitles, productsPrices, finalPrice, orderDate, orderStatus);
+                newOrder.insertOne()
+                .then(
+                    Order.fetchAll().then(r => console.log(r[0]))
+                )
+            }
+            else {
+                console.log('shhes');
+                // res.status(400).send('There is a problem with fetching data from the database.')
+            }
+        })
+        
         // TODO: insert data to database
         // TODO: if succes: show ok
         // TODO: clear form and localstorage on frontend
         res.status(200).send('ok')
     }
     else {
-        res.status(400).send('bad data')
+        res.status(400).send('You have passed a bad data!')
     }
     
 }
--- NodeJS/Exercises/simple-online-shop/models/Order.js
+++ NodeJS/Exercises/simple-online-shop/models/Order.js
@@ -1,13 +1,29 @@
 const db = require('../utilities/database');
 
 class Order {
-    constructor(id, customerName, customerSurname, productsIDs, productsQTYs, orderDate) {
+    constructor(id, customerName, customerSurname, productsIDs, productsQTYs, productsTitles, productsPrices, finalPrice, orderDate, orderStatus) {
         this.id = id;
         this.customerName = customerName;
         this.customerSurname = customerSurname;
         this.productsIDs = productsIDs;
         this. productsQTYs = productsQTYs;
-        this.orderDate = orderDate;
+        this.productsTitles = productsTitles;
+        this.productsPrices = productsPrices;
+        this.finalPrice = finalPrice;
+        this.orderDate = new Date(orderDate).toISOString().replace('T', ' ').replace('Z', '');
+        this.orderStatus = orderStatus;
+    }
+
+
+    static fetchAll() {
+        return db.execute('SELECT * FROM `orders`');
+    }
+    insertOne = () => {
+        return db.execute('INSERT INTO `orders` (`customerName`, `customerSurname`, `productsIDs`, `productsQTYs`, `productsTitles`, `productsPrices`, `finalPrice`, `orderDate`, `orderStatus`) VALUES (?,?,?,?,?,?,?,?,?)', [this.customerName, this.customerSurname, this.productsIDs, this.productsQTYs, this.productsTitles, this.productsPrices, this.finalPrice, this.orderDate, this.orderStatus]);
     }
 
 }
+
+
+
+module.exports = Order;
