8beda14e2fa03d527b1e714269a5b9b1b6a553b8
jbundziow <jakub.bundziow@gmail.com>
2023-08-24 10:47:33 +0200

    added some tests

--- NodeJS/Exercises/simple-online-shop/models/Product.test.js
+++ NodeJS/Exercises/simple-online-shop/models/Product.test.js
@@ -1,18 +1,81 @@
-import {describe, it, expect, vi} from 'vitest'
+import {describe, it, expect, vi, beforeEach} from 'vitest'
 const Product = require('./Product');
 const db = require('../utilities/database');
 
-vi.mock('db');
+beforeEach(()=> {
+    vi.mock('db');
+})
 
-it('sdds', async () => {
-    // Mock the db.execute function to return a sample result
-    db.execute = vi.fn(() => Promise.resolve({ rows: [{ id: 1, title: 'Product A' }, { id: 2, title: 'Product B' }] }));
+describe('Product constructor test', () => {
+    it('should create new instance of Product', () => {
+        const prod = new Product('2', 'title', 'desc', 22);
+        
 
+        
+        expect(prod).toBeInstanceOf(Product)
 
-     const products = await Product.fetchAll();
-    // const spy = vi.spyOn(db, 'execute')
-    expect(db.execute).toHaveBeenCalledWith('SELECT * FROM `products`')
-    expect(products).toEqual({ rows: [{ id: 1, title: 'Product A' }, { id: 2, title: 'Product B' }] })
-    // console.log(x[0].length);
-    db.execute.mockClear();
+        expect(prod).toHaveProperty('id')
+        expect(prod.id).toBe('2')
+
+        expect(prod).toHaveProperty('title')
+        expect(prod.title).toBe('title')
+
+        expect(prod).toHaveProperty('description')
+        expect(prod.description).toBe('desc')
+
+        expect(prod).toHaveProperty('price')
+        expect(prod.price).toBe(22)
+
+    })
+})
+
+describe('Product.fetchAll() test', () => {
+    it('should fetch data from db', async () => {
+        const correctResponse =   [[
+            {
+              id: 6,
+              title: 'product1',
+              description: 'desc1',
+              price: 22
+            },
+            {
+                id: 7,
+                title: 'product2',
+                description: 'desc2',
+                price: 23.83
+              },
+          ]]
+      
+        db.execute = vi.fn(() => Promise.resolve(correctResponse));
+        const products = await Product.fetchAll();
+
+        expect(db.execute).toHaveBeenCalledWith('SELECT * FROM `products`')
+        expect(products).toEqual(correctResponse)
+
+        db.execute.mockClear();
+    })
+
+    it('should catch error while promise is rejected', async () => {
+      
+        db.execute = vi.fn(() => Promise.reject('some error...'));
+
+        await expect(Product.fetchAll()).rejects.toBe('some error...');
+        expect(db.execute).toHaveBeenCalledWith('SELECT * FROM `products`')
+     
+        db.execute.mockClear();
+    })
+
+    it('should catch error while promise is rejected ver2 (try/catch)', async () => {
+      
+        db.execute = vi.fn(() => Promise.reject('error'));
+
+        try{
+        const prods = await Product.fetchAll();
+        }
+        catch (err) {
+            expect(err).toBe('error')
+        }
+
+        db.execute.mockClear();
+    })
 })
