200b932c396ceef4df00ac2add0b251c7c431afe
jbundziow <jakub.bundziow@gmail.com>
2023-08-29 22:50:33 +0200

    added 6 exercises

---
+++ Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise1.ts
@@ -0,0 +1,7 @@
+// Exercise 1: Shape Hierarchy
+/*
+Create a hierarchy of shapes using classes in TypeScript. Start with an abstract class Shape that has a method to calculate area.
+Then, create concrete classes like Circle, Rectangle, and Triangle that inherit from Shape and provide their specific implementations for calculating area.
+*/
+
+
