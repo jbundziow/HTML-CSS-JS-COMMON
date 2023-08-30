150c8ec7fe77c86bb5ebd3549e1efd0a398064e1
jbundziow <jakub.bundziow@gmail.com>
2023-08-30 16:55:32 +0200

    completed exercise4.ts and exercise5.ts

--- Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise4.ts
+++ Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise4.ts
@@ -4,3 +4,54 @@ Design a class hierarchy for vehicles. Start with a base class Vehicle with prop
 Create subclasses like Car, Motorcycle, and Truck, each with specific methods or properties.
 Demonstrate how polymorphism works by calling the startEngine() method on different types of vehicles.
 */
+
+class Vehicle {
+    constructor(private make: string, private model: string) {}
+
+    startEngine(): void {
+        console.log(`${this.make} ${this.model}'s engine started.`);
+    }
+}
+
+class Carr extends Vehicle {
+    constructor(make: string, model: string) {
+        super(make, model);
+    }
+
+    drive(): void {
+        console.log('Car is driving.');
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make: string, model: string) {
+        super(make, model);
+    }
+
+    wheelie(): void {
+        console.log('Motorcycle is performing a wheelie.');
+    }
+}
+
+class Truck extends Vehicle {
+    constructor(make: string, model: string) {
+        super(make, model);
+    }
+
+    loadCargo(): void {
+        console.log('Truck is loading cargo.');
+    }
+}
+
+const carr = new Carr('Toyota', 'Camry');
+const motorcycle = new Motorcycle('Harley-Davidson', 'Sportster');
+const truck = new Truck('Ford', 'F-150');
+
+carr.startEngine();
+carr.drive();
+
+motorcycle.startEngine();
+motorcycle.wheelie();
+
+truck.startEngine();
+truck.loadCargo();
--- Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise5.ts
+++ Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise5.ts
@@ -4,3 +4,46 @@ Build a basic school management system. Create a class Person with properties li
 Derive classes like Student and Teacher from Person, adding relevant properties like grade and subject.
 Implement a method to display information for each person.
 */
+
+class Person {
+    constructor(private name: string, private age: number) {}
+
+    displayInfo(): void {
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+
+    get personName(): string {
+        return this.name;
+    }
+    get personAge(): number {
+        return this.age;
+    }
+}
+
+class Student extends Person {
+    constructor(name: string, age: number, private grade: number) {
+        super(name, age);
+    }
+
+    displayInfo(): void {
+        super.displayInfo();
+        console.log(`Grade: ${this.grade}`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name: string, age: number, private subject: string) {
+        super(name, age);
+    }
+
+    displayInfo(): void {
+        console.log(`Name: ${this.personName}, Age: ${this.personAge}, Subject: ${this.subject}`);
+    }
+}
+
+
+const student = new Student('Alice', 15, 10);
+const teacher = new Teacher('Mr. Smith', 35, 'Math');
+
+student.displayInfo();
+teacher.displayInfo();
