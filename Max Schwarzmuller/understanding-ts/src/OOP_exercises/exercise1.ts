ab8fe2a7f0fd3d2fdc5883e5cd3388e8344b4158
jbundziow <jakub.bundziow@gmail.com>
2023-08-30 08:43:53 +0200

    exercise1 is finished

--- Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise1.ts
+++ Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise1.ts
@@ -6,19 +6,66 @@ Then, create concrete classes like Circle, Rectangle, and Triangle that inherit 
 
 
 abstract class Shape {
-    constructor(public a: number) {
-        this.a = a;
-    }
     abstract calculateArea(): number;
 }
 
 class Rectangle extends Shape {
+    constructor (private readonly a: number, private readonly b: number) {
+        super();
+        if (a >= 0 && b >= 0) {
+            this.a = a;
+            this.b = b;
+        }
+        else {
+            throw new Error('Rectangle cannot contain negative values!')
+        }
+        
+    }
+
     calculateArea(): number {
-        return this.a;
+        return this.a*this.b;
+    }
+}
+
+class Circle extends Shape {
+    constructor(private readonly r: number) {
+        super();
+        if (r >= 0) {
+        this.r = r;
+        }
+        else {
+            throw new Error('Radius cannot be negative!')
+        }
+    }
+
+    calculateArea(): number {
+        return Math.PI*Math.pow(this.r, 2);
+    }
+}
+
+class Triangle extends Shape {
+    constructor(private readonly a: number, private readonly h: number) {
+        super();
+        if (a >= 0 && h >= 0) {
+        this.a = a;
+        this.h = h;
+        }
+        else {
+            throw new Error('Triangle cannot contain negative values!')
+        }
+    }
+
+    calculateArea(): number {
+        return this.a * this.h /2;
     }
 }
 
 
-const x = new Rectangle(5);
-const y = x.calculateArea();
-console.log(y);
+const rectangle = new Rectangle(5,4);
+console.log(rectangle.calculateArea());
+
+const circle = new Circle(2);
+console.log(circle.calculateArea());
+
+const triangle = new Triangle(2,3);
+console.log(triangle.calculateArea());
--- Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise2.ts
+++ Max Schwarzmuller/understanding-ts/src/OOP_exercises/exercise2.ts
@@ -4,3 +4,34 @@ Build a simple banking system using TypeScript classes.
 Implement an abstract class BankAccount with encapsulated balance and methods for deposit and withdrawal.
 Create subclasses SavingsAccount and CheckingAccount with specific interest rates and overdraft limits.
 */
+
+abstract class BankAccount {
+    // abstract balance: number;
+    abstract deposit(amount: number): void;
+    abstract withdrawal(amount: number): void;
+}
+
+class CheckingAccount extends BankAccount {
+    constructor (private balance: number) {
+        super();
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        this.balance += amount;
+        console.log(`You\'ve successfully deposited funds. Your balance now is: ${this.balance}`);
+    }
+
+    withdrawal(amount: number): void {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(`You\'ve successfully withdrawn funds. Your balance now is: ${this.balance}`);
+        }
+        else {
+            console.log('You don\'t have enough money on your bank account!')
+        }
+    }
+}
+
+const account1 = new CheckingAccount(120.22);
+console.log(account1.deposit(122));
