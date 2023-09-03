d493e8331d7572ab7399d394956afb6906c6fcd3
jbundziow <jakub.bundziow@gmail.com>
2023-09-03 13:39:04 +0200

    displaying form works

--- Max Schwarzmuller/typescript-drag-and-drop-project/src/app.ts
+++ Max Schwarzmuller/typescript-drag-and-drop-project/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+
+
+class ProjectInput {
+    public templateElement: HTMLTemplateElement;
+    public hostElement: HTMLDivElement;
+    public element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.querySelector('#project-input') as HTMLTemplateElement;
+        this.hostElement = document.querySelector('#app') as HTMLDivElement;
+        
+        const importedNode = document.importNode(this.templateElement.content, true); //import template from <template> tag
+        this.element = importedNode.firstElementChild as HTMLFormElement; //get <form> from inside of <template> tag
+        
+        this.attach()
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element) //attach form into <div> #app
+    }
+}
+
+const prjInput = new ProjectInput();
